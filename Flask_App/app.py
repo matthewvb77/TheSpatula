9495b13d7dcfe309c2999e71f054b79545b48353
Matthew Van Brummelen <vb.matthew@gmail.com>
2021-11-12 18:31:50 -0800

    starting flask tutorial

--- Flask_App/app.py
+++ Flask_App/app.py
@@ -2,7 +2,11 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
 	name = request.args.get("name", "default")
-	return render_template("index.html", name=name)
+	return render_template("tut2.html", name=name)
+
+@app.route("/register", methods=["POST"])
+def register():
+	
