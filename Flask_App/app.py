00d6aa5192ffffcbac1860c732e631926694af1e
Matthew Van Brummelen <vb.matthew@gmail.com>
2021-11-16 14:13:12 -0800

    started customizing the 'Architects template'

--- Flask_App/app.py
+++ Flask_App/app.py
@@ -2,11 +2,8 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
 	name = request.args.get("name", "default")
-	return render_template("tut2.html", name=name)
-
-@app.route("/register", methods=["POST"])
-def register():
-	
+	return render_template("index.html", name=name)
