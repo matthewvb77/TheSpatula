538826dec099f3df7af1dbc4a3f4e280fb7402a8
Matthew Van Brummelen <mvanbrummelen@uvic.ca>
2021-12-16 13:42:07 -0800

    Flask app unknown changes

--- Flask_App/app.py
+++ Flask_App/app.py
@@ -2,11 +2,17 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-	name = request.args.get("name", "default")
-	return render_template("tut2.html", name=name)
+    name = request.args.get("name", "default")
+    return render_template("index.html", name=name)
+
 
 @app.route("/register", methods=["POST"])
 def register():
-	
+    name = request.form.get("name")
+    dorm = request.form.get("dorm")
+    if not name or not dorm:
+        return "failure"
+    return render_template("success.html")
