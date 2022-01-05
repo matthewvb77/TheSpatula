73ef0e229352d60a5dd98930396daafbfa99cea2
Matthew Van Brummelen <mvanbrummelen@uvic.ca>
2022-01-04 23:15:37 -0800

    added DB access for ticker list (not working)

--- Flask_App/app.py
+++ Flask_App/app.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+from Scripts import DatabaseHandler
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-	name = request.args.get("name", "default")
-	return render_template("index.html", name=name)
+	user = DatabaseHandler.User("my_win")
+	cnx = DatabaseHandler.connect_to_db(user, "TheSpatula")
+	mycursor = cnx.cursor()
+	assert mycursor
+	assert DatabaseHandler.table_exists(mycursor, "crypto_symbols")
+	mycursor.execute(f"""SELECT symbol FROM crypto_symbols;""")
+	ticker_list = list()
+	for row in mycursor:
+		ticker_list.append(row[0])
+
+	return render_template("index.html", ticker_list=ticker_list)
